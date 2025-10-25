cf7ba543d0f176df4e010f6415caae312bb5e652
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-10-25 20:26:22 +0900

    refactor

--- src/main.ts
+++ src/main.ts
@@ -87,22 +87,21 @@ function getBangredirectUrl() {
 
   const bangCandidate = match?.[1]?.toLowerCase();
   const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+  if (!selectedBang) return null;
 
   // Remove the first bang from the query
   const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
 
   // If the query is just `!gh`, use `github.com` instead of `github.com/search?q=`
-  if (cleanQuery === "")
-    return selectedBang ? `https://${selectedBang.d}` : null;
+  if (cleanQuery === "") return `https://${selectedBang.d}`;
 
   // Format of the url is:
   // https://www.google.com/search?q={{{s}}}
-  const searchUrl = selectedBang?.u.replace(
+  const searchUrl = selectedBang.u.replace(
     "{{{s}}}",
     // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
     encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
   );
-  if (!searchUrl) return null;
 
   return searchUrl;
 }
