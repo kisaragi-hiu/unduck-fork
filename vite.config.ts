dd8534512aad7ffde15806fea1900da6e9c5f01d
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-10-25 19:29:31 +0900

    misc: comments for if the bangs bundle becomes too big

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  // build: {
+  //   chunkSizeWarningLimit: 3000,
+  // },
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
+      // workbox: { maximumFileSizeToCacheInBytes: 3 * 1024 * 1024 },
     }),
   ],
 });
